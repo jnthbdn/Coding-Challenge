ef48c4b24ec700fa26cf3e70d519158351cb65bf
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-12-13 23:19:54 +0100

    add seek

--- LOGO/tokenReader.js
+++ LOGO/tokenReader.js
@@ -4,9 +4,21 @@ class TokenReader {
         this.index = 0;
         this.text = (text + " ").replaceAll("[", "[ ")          // Be sure to "tokenize" [
                                 .replaceAll("]", " ]")          // Be sure to "tokenize" ]
-                                .replaceAll(/;.*$/gm, " ")       // Remove comments
+                                .replaceAll(/;.*$/gm, " ")      // Remove comments
                                 .replaceAll(/\s{2,}/g, " ")     // Remove multiple whitespace
                                 .replaceAll(/\s/g, " ");        // Replace all whitespace with a space
+    }
+
+    seekNextToken(separator = " "){
+        let nextIndex = this.text.indexOf(separator, this.index);
+        
+        if( nextIndex == -1){
+            return new TokenReaderError(TokenReaderError.SEPARATOR_NOT_FOUND);
+        }
+    
+        let token = this.text.substring( this.index, nextIndex);
+        
+        return token;
     }
 
     nextToken(separator = " "){
@@ -16,7 +28,7 @@ class TokenReader {
             return new TokenReaderError(TokenReaderError.SEPARATOR_NOT_FOUND);
         }
     
-        let token = this.text.substring( this.index, nextIndex + separator.length);
+        let token = this.text.substring( this.index, nextIndex);
         
         this.index = nextIndex + separator.length;
         return token;
@@ -28,15 +40,15 @@ class TokenReader {
 
         if( a instanceof TokenReaderError ){ return new TokenReaderError(TokenReaderError.CONTAINER_NOT_FOUND); }
 
-        if( !a.trim().startsWith(open) ){
-            return new TokenReaderError(TokenReaderError.CONTAINER_NOT_NEXT, a.substring(0, a.indexOf(open)));
+        if( a.length > 0 ){
+            return new TokenReaderError(TokenReaderError.CONTAINER_NOT_NEXT, a);
         }
 
         let b = this.nextToken(close);
 
         if( b instanceof TokenReaderError ){ return new TokenReaderError(TokenReaderError.CONTAINER_CLOSE_NOT_FOUND); }
         
-        return b.substring(0, b.length - close.length).trim();
+        return b.substring(0, b.length).trim();
     }
 
     isParseFinish(){
